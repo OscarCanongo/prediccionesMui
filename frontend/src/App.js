5bdcc14fe44655910a7c8513fb06e0eb79490325
Alkiva <fernanda22mr@gmail.com>
2020-11-13 11:24:43 -0600

    matrix background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Prediccion from './components/Prediccion/Prediccion';
-import { Fragment } from 'react';
+import React, { Fragment } from "react";
+import MatrixParallax from "react-matrix-parallax";
 
 function App() {
   return (
-    <Fragment>
-      <Prediccion/>
-   </Fragment>
+    <>
+      <div className="App">
+        <Fragment>
+          <Prediccion/>
+        </Fragment>
+        <React.Fragment>
+          <MatrixParallax
+            color="rgba(122, 229, 114, 0.87)"
+            backgroundColor="rgba(0,0,0,1)"
+          >
+          </MatrixParallax>
+        </React.Fragment>
+        
+      </div>
+
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import MatrixParallax from 'react-matrix-parallax'
-import Terminal from 'terminal-in-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MatrixParallax color='rgba(37, 218, 28, 0.87)' backgroundColor='rgba(0,0,0,1)'>
-    <Terminal commands={{ 'open-google': "https://www.google.com/"}} />
-    </MatrixParallax>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
