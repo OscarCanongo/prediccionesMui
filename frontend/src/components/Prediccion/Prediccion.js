74239046cf4d367c19e7a9d8ffca95c381b2182b
Alkiva <fernanda22mr@gmail.com>
2020-11-13 12:57:09 -0600

    terminal centrada

--- frontend/src/components/Prediccion/Prediccion.js
+++ frontend/src/components/Prediccion/Prediccion.js
@@ -5,7 +5,16 @@ import Terminal from 'terminal-in-react';
 const Prediccion = () => {
 
     return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh"
+        }}
+      >
       <Terminal commands={{ 'open-google': () => window.open("https://www.google.com/", "_blank")}} />
+      </div>
     );
 }
 
