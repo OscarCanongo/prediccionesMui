0594cfed678d36ad57e24bcfd98856ccbd35f6c0
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2020-11-13 10:44:48 -0600

    Prediccion usuario inicio

---
+++ frontend/src/components/Prediccion/Prediccion.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Prediccion from '../Prediccion.css';
+
+const Prediccion = () => {
+
+    return (
+        
+        <div className="container-sm">
+        <div className="row">
+          <div className="col-sm">
+            <h1 className="text-center">Prediccion</h1>
+          </div>
+        </div>
+        </div>
+            
+
+            
+    );
+}
+
+export default Prediccion;
