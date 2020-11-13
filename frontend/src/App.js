9199e0c1aae70f980e1e1fda825b08403172774f
Alkiva <fernanda22mr@gmail.com>
2020-11-13 11:56:05 -0600

    random Matrix background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
 import Prediccion from './components/Prediccion/Prediccion';
 import React, { Fragment } from "react";
-import MatrixParallax from "react-matrix-parallax";
+import Matrix from './components/Matrix/Matrix';
 
 function App() {
   return (
     <>
       <div className="App">
         <Fragment>
+          <Matrix/>
           <Prediccion/>
         </Fragment>
-        <React.Fragment>
-          <MatrixParallax
-            color="rgba(122, 229, 114, 0.87)"
-            backgroundColor="rgba(0,0,0,1)"
-          >
-          </MatrixParallax>
-        </React.Fragment>
         
       </div>
 
---
+++ frontend/src/components/Matrix/Matrix.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Code from './Code';
+import './Matrix.css';
+import _ from 'lodash';
+import FadeText from './AnimateText';
+
+const CODE_LINES_COUNT = 100;
+
+const Matrix = () => (
+  <div className="Matrix">
+    {_.times(CODE_LINES_COUNT).map((_, i) => <Code key={i} />)}
+  </div>
+);
+
+
+export default Matrix;
