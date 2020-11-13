c3d06211214e5f2fd2cd522dcb98dd99ef4b3fc6
Alkiva <fernanda22mr@gmail.com>
2020-11-13 10:53:08 -0600

    terminal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import MatrixParallax from 'react-matrix-parallax'
+import Terminal from 'terminal-in-react';
 
 ReactDOM.render(
   <React.StrictMode>
     <MatrixParallax color='rgba(37, 218, 28, 0.87)' backgroundColor='rgba(0,0,0,1)'>
+    <Terminal commands={{ 'open-google': "https://www.google.com/"}} />
     </MatrixParallax>
     <App />
   </React.StrictMode>,
