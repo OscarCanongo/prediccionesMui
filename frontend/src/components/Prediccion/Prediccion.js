f882ffd04beb0daf947b5051c5f8870ffc26c54e
Alkiva <fernanda22mr@gmail.com>
2020-11-25 11:02:49 -0600

    Preguntas

--- frontend/src/components/Prediccion/Prediccion.js
+++ frontend/src/components/Prediccion/Prediccion.js
@@ -41,13 +41,62 @@ const Prediccion = () => {
       </Row>
       <Row>
         <Form>
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group >
             <Form.Label>Nombre</Form.Label>
             <Form.Control placeholder="Nombre" />
           </Form.Group>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Correo electronico</Form.Label>
-            <Form.Control placeholder="Correo" />
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Edad</Form.Label>
+            <Form.Control as="select">
+              <option>18</option>
+              <option>19</option>
+              <option>20</option>
+              <option>21</option>
+              <option>22</option>
+              <option>23</option>
+              <option>24</option>
+              <option>25</option>
+              <option>26</option>
+              <option>27</option>
+              <option>28</option>
+              <option>29</option>
+              <option>30</option>
+              <option>31</option>
+              <option>32</option>
+              <option>33</option>
+              <option>34</option>
+              <option>35</option>
+              <option>36</option>
+              <option>37</option>
+              <option>38</option>
+              <option>39</option>
+              <option>40</option>
+              <option>41</option>
+              <option>42</option>
+              <option>43</option>
+              <option>44</option>
+              <option>45</option>
+              <option>46</option>
+              <option>47</option>
+              <option>48</option>
+              <option>49</option>
+              <option>50</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Posición Social</Form.Label>
+            <Form.Control as="select">
+              <option>Baja</option>
+              <option>Media</option>
+              <option>Alta</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Edad</Form.Label>
+            <Form.Control as="select">
+              <option>Mujer</option>
+              <option>Hombre</option>
+            </Form.Control>
           </Form.Group>
           <Button variant="primary">Primary</Button>
         </Form>
