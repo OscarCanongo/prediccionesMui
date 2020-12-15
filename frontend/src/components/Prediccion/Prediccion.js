d5e6995e1e69f29d6310a8612383a6df0a7ef5a9
Jesus Librado <jesusl@sora.mx>
2020-12-15 17:34:47 -0600

    Tried to fix layout, but it is already fucked with the Matrix alike wrapper style sheet

--- frontend/src/components/Prediccion/Prediccion.js
+++ frontend/src/components/Prediccion/Prediccion.js
@@ -1,14 +1,11 @@
 import React, {useState, useEffect, Fragment} from 'react';
 import './Prediccion.css';
-import Terminal from 'terminal-in-react';
 import clienteAxios from '../../config/axios';
-import {Button, Form, Container, Row } from 'react-bootstrap';
-import { green, lightGreen} from '@material-ui/core/colors';
-import { useForm } from 'react-hook-form'
+import {Button, Col, Container, Row } from 'react-bootstrap';
+import PerfectScrollbar from 'react-perfect-scrollbar';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { unstable_createMuiStrictModeTheme } from '@material-ui/core';
 const Prediccion = () => {
 
   //Bandera
@@ -31,25 +28,11 @@ const Prediccion = () => {
         location: ''
     });
 
-    //Extraer de de usuario
-    const {nombre, edad, posicion, genero} = usuario;
-
-    //Extraer de de prediccion
-    const {year, location} = usuario;
-
-    const onChange1 = (e) =>{
-      setPrediccion({
-          ...prediccion,
-          [e.target.name] : e.target.value
-      })
-    };
 
     //Use Effect
     useEffect(() => {
       //console.log("ENTRA");
     },[showPrediction]);
-
-    const {register, errors, handleSubmit} = useForm();
 
     //Cuando el usuaro quiere iniciar sesion
     const onSubmit = async (e) => {
@@ -57,7 +40,6 @@ const Prediccion = () => {
         const resp = e.target.value;
         if(resp === 'si'){
           setShowPrediction(true);
-          console.log({usuario});
           if(usuario.posicion.toLowerCase() === 'alto'){
             usuario.posicion = 3;
           } else if (usuario.posicion.toLowerCase === 'medio'){
@@ -90,23 +72,6 @@ const Prediccion = () => {
       }
     }
 
-    const onSubmitPrediccion = async (e) => {
-      e.preventDefault();
-      const response = await clienteAxios.post('/predicciones/prediccion',
-              {
-                date : prediccion.year,
-                location: prediccion.location
-              }
-            );
-            setData(response.data.prediccion[0].text);
-    }
-
-    const onSubmit1 = e => {
-      e.preventDefault();
-
-      console.log({prediccion});
-    }
-
     const setValue = e =>{
       if (e.key === "Enter") {
         setUsuario({
@@ -135,207 +100,211 @@ const Prediccion = () => {
         }}
       >
 
-        <Container className="">
-         {
-           (!showPrediction)?
-           <div className="cmd">
-            <div className="line"> 
-              <span className="header">
-                &#36; root&#64;M.U.I&#62; Llene el formulario para conocer el futuro
-              </span>
-            </div>
-            <div className="line"> 
-              <span className="header">
-                &#36; root&#64;M.U.I&#62; Cargando datos...
-              </span>
-            </div>
-            <div className="line"> 
-              <span className="header">
-              &#36; root&#64;M.U.I&#62;
-              </span>
-            </div>
-            <div className="line"> 
-              <span className="header">
-              &#36; Nombre:
-              </span>
-              <div className="code">
-                <input type="text" name="nombre" id="textinput" onKeyPress={setValue}/>
-              </div>
-            </div>
-            {
-              (usuario.nombre && usuario.nombre.length>1)?(
-              <div>
-                <div className="line"> 
-                  <span className="header">
-                    &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo nombre...
-                  </span>
-                </div>
-                <div className="line"> 
-                  <span className="header">
-                    &#36; {usuario.nombre}&#64;M.U.I&#62;
-                  </span>
-                </div>
-                <div className="line"> 
-                  <span className="header">
-                  &#36; Edad:
-                  </span>
-                  <div className="code">
-                    <input type="text" name="edad" id="textinput" onKeyPress={setValue}/>
-                  </div>
-                </div>
-              </div>
-              ):null
-            }
-            {
-              (usuario.edad && usuario.edad>0)?(
-              <div>
-                <div className="line"> 
-                  <span className="header">
-                    &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo edad...
-                  </span>
-                </div>
-                <div className="line"> 
-                  <span className="header">
-                    &#36; {usuario.nombre}&#64;M.U.I&#62;
-                  </span>
-                </div>
-                <div className="line"> 
-                  <span className="header">
-                  &#36; Género (masculino o femenino):
-                  </span>
-                  <div className="code">
-                    <input type="text" name="genero" id="textinput" onKeyPress={setValue}/>
-                  </div>
-                </div>
-              </div>
-              ):null
-            }
-            {
-              (usuario.genero && (usuario.genero==='masculino' || usuario.genero==='femenino'))?(
-              <div>
-                <div className="line"> 
-                  <span className="header">
-                    &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo género...
-                  </span>
-                </div>
-                <div className="line"> 
-                  <span className="header">
-                    &#36; {usuario.nombre}&#64;M.U.I&#62;
-                  </span>
-                </div>
-                <div className="line"> 
-                  <span className="header">
-                  &#36; Posición socioeconómica (alto, bajo o medio):
-                  </span>
-                  <div className="code">
-                    <input type="text" name="posicion" id="textinput" onKeyPress={setValue}/>
-                  </div>
-                </div>
-              </div>
-              ):null
-            }
-            {
-              (usuario.genero && (
-                  usuario.posicion==='alto' || 
-                  usuario.posicion==='medio' ||
-                  usuario.posicion==='bajo'
-                )
-              )?
-              (
-                <div>
+        <Container fluid="md">
+          <Row>
+            <Col md={6} lg={6}>
+              <PerfectScrollbar scrollYMarginOffset="300">
+                <div className="cmd">
                   <div className="line"> 
                     <span className="header">
-                      &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo posición...
+                      &#36; root&#64;M.U.I&#62; Llene el formulario para conocer el futuro
                     </span>
                   </div>
                   <div className="line"> 
                     <span className="header">
-                      &#36; {usuario.nombre}&#64;M.U.I&#62;
+                      &#36; root&#64;M.U.I&#62; Cargando datos...
                     </span>
                   </div>
                   <div className="line"> 
                     <span className="header">
-                    &#36; Predicción (Mexico o Mundo):
+                    &#36; root&#64;M.U.I&#62;
+                    </span>
+                  </div>
+                  <div className="line"> 
+                    <span className="header">
+                    &#36; Nombre:
                     </span>
                     <div className="code">
-                      <input type="text" name="location" id="textinput" onKeyPress={setValuePrediccion}/>
+                      <input type="text" name="nombre" id="textinput" onKeyPress={setValue}/>
                     </div>
                   </div>
-                </div>
-              ):null
-            }
-            {
-              (prediccion.location && (
-                prediccion.location.toLowerCase()==='mexico' || 
-                prediccion.location.toLowerCase()==='mundo' 
-                )
-              )?
-              (
-                <div>
-                  <div className="line"> 
-                    <span className="header">
-                      &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo hubicación...
-                    </span>
-                  </div>
-                  <div className="line"> 
-                    <span className="header">
-                      &#36; {usuario.nombre}&#64;M.U.I&#62;
-                    </span>
-                  </div>
-                  <div className="line"> 
-                    <span className="header">
-                    &#36; Año (2030 o 2040):
-                    </span>
-                    <div className="code">
-                      <input type="text" name="year" id="textinput" onKeyPress={setValuePrediccion}/>
+                  {
+                    (usuario.nombre && usuario.nombre.length>1)?(
+                    <div>
+                      <div className="line"> 
+                        <span className="header">
+                          &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo nombre...
+                        </span>
+                      </div>
+                      <div className="line"> 
+                        <span className="header">
+                          &#36; {usuario.nombre}&#64;M.U.I&#62;
+                        </span>
+                      </div>
+                      <div className="line"> 
+                        <span className="header">
+                        &#36; Edad:
+                        </span>
+                        <div className="code">
+                          <input type="text" name="edad" id="textinput" onKeyPress={setValue}/>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              ):null
-              }
-              {
-              (prediccion.year && (
-                prediccion.year === '2030' || 
-                prediccion.year=== '2040' 
-                )
-              )?
-              (
-                <div>
-                  <div className="line"> 
-                    <span className="header">
-                      &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo año...
-                    </span>
-                  </div>
-                  <div className="line"> 
-                  <span className="header">
-                    &#36; {usuario.nombre}&#64;M.U.I&#62;
-                  </span>
-                </div>
+                    ):null
+                  }
+                  {
+                    (usuario.edad && usuario.edad>0)?(
+                    <div>
+                      <div className="line"> 
+                        <span className="header">
+                          &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo edad...
+                        </span>
+                      </div>
+                      <div className="line"> 
+                        <span className="header">
+                          &#36; {usuario.nombre}&#64;M.U.I&#62;
+                        </span>
+                      </div>
+                      <div className="line"> 
+                        <span className="header">
+                        &#36; Género (masculino o femenino):
+                        </span>
+                        <div className="code">
+                          <input type="text" name="genero" id="textinput" onKeyPress={setValue}/>
+                        </div>
+                      </div>
+                    </div>
+                    ):null
+                  }
+                  {
+                    (usuario.genero && (usuario.genero==='masculino' || usuario.genero==='femenino'))?(
+                    <div>
+                      <div className="line"> 
+                        <span className="header">
+                          &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo género...
+                        </span>
+                      </div>
+                      <div className="line"> 
+                        <span className="header">
+                          &#36; {usuario.nombre}&#64;M.U.I&#62;
+                        </span>
+                      </div>
+                      <div className="line"> 
+                        <span className="header">
+                        &#36; Posición socioeconómica (alto, bajo o medio):
+                        </span>
+                        <div className="code">
+                          <input type="text" name="posicion" id="textinput" onKeyPress={setValue}/>
+                        </div>
+                      </div>
+                    </div>
+                    ):null
+                  }
+                  {
+                    (usuario.genero && (
+                        usuario.posicion==='alto' || 
+                        usuario.posicion==='medio' ||
+                        usuario.posicion==='bajo'
+                      )
+                    )?
+                    (
+                      <div>
+                        <div className="line"> 
+                          <span className="header">
+                            &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo posición...
+                          </span>
+                        </div>
+                        <div className="line"> 
+                          <span className="header">
+                            &#36; {usuario.nombre}&#64;M.U.I&#62;
+                          </span>
+                        </div>
+                        <div className="line"> 
+                          <span className="header">
+                          &#36; Predicción (Mexico o Mundo):
+                          </span>
+                          <div className="code">
+                            <input type="text" name="location" id="textinput" onKeyPress={setValuePrediccion}/>
+                          </div>
+                        </div>
+                      </div>
+                    ):null
+                  }
+                  {
+                    (prediccion.location && (
+                      prediccion.location.toLowerCase()==='mexico' || 
+                      prediccion.location.toLowerCase()==='mundo' 
+                      )
+                    )?
+                    (
+                      <div>
+                        <div className="line"> 
+                          <span className="header">
+                            &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo hubicación...
+                          </span>
+                        </div>
+                        <div className="line"> 
+                          <span className="header">
+                            &#36; {usuario.nombre}&#64;M.U.I&#62;
+                          </span>
+                        </div>
+                        <div className="line"> 
+                          <span className="header">
+                          &#36; Año (2030 o 2040):
+                          </span>
+                          <div className="code">
+                            <input type="text" name="year" id="textinput" onKeyPress={setValuePrediccion}/>
+                          </div>
+                        </div>
+                      </div>
+                    ):null
+                    }
+                    {
+                    (prediccion.year && (
+                      prediccion.year === '2030' || 
+                      prediccion.year=== '2040' 
+                      )
+                    )?
+                    (
+                      <div>
+                        <div className="line"> 
+                          <span className="header">
+                            &#36; {usuario.nombre}&#64;M.U.I&#62; Leyendo año...
+                          </span>
+                        </div>
+                        <div className="line"> 
+                        <span className="header">
+                          &#36; {usuario.nombre}&#64;M.U.I&#62;
+                        </span>
+                      </div>
+                      <div className="line"> 
+                        <span className="header">
+                        &#36; ¿Desea guardar los datos de usuario? (si o no):
+                        </span>
+                        <div className="code">
+                          <input type="text" name="send" id="textinput" onKeyPress={onSubmit}/>
+                        </div>
+                      </div>
+                      </div>
+                    ):null
+                    }
+                </div> 
+              </PerfectScrollbar>
+            </Col>
+            <Col md={6} lg={6}>
+              <div className="cmd">
                 <div className="line"> 
                   <span className="header">
-                  &#36; ¿Desea guardar los datos de usuario? (si o no):
+                    Predicción para {prediccion.location}, el {prediccion.year}:
                   </span>
-                  <div className="code">
-                    <input type="text" name="send" id="textinput" onKeyPress={onSubmit}/>
+                  <br></br>
+                  <div className="code" style={{fontWeight: "bold", margin: '20px 0px'}}>
+                    {data}
                   </div>
                 </div>
-                </div>
-              ):null
-              }
-          </div> : 
-          <div className="cmd">
-            <div className="line"> 
-              <span className="header">
-                Predicción para {prediccion.location}, el {prediccion.year}:
-              </span>
-              <br></br>
-              <div className="code" style={{fontWeight: "bold", margin: '20px 0px'}}>
-                {data}
               </div>
-            </div>
-          </div>
-         }
-
+            </Col>
+          </Row>
         </Container>
       </div>
       </>
